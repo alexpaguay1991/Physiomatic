c0d374b9b75f42beda2486bf7817a4682519e3bf
= <75266819+alexpaguay1991@users.noreply.github.com>
2021-03-01 22:24:25 -0500

    "upload createpatient"

--- 06Code/Physiomatic_v0.9.1/src/ec/edu/espe/physiomatic/view/FrmCreatePatient.java
+++ 06Code/Physiomatic_v0.9.1/src/ec/edu/espe/physiomatic/view/FrmCreatePatient.java
@@ -189,6 +189,7 @@ public class FrmCreatePatient extends javax.swing.JFrame {
         int selection = JOptionPane.showConfirmDialog(null, null, "¿DESEA REGISTRAR EL PACIENTE?", JOptionPane.YES_NO_CANCEL_OPTION);
         if (selection == 0) {
             JOptionPane.showMessageDialog(null, "SU INFORMACIÓN HA SIDO GUARDADA CON ÉXITO", txtLastName + "PACIENTE REGISTRADO", JOptionPane.INFORMATION_MESSAGE);
+            register();
             emptyFields();
                           
         } else if (selection == 1) {
@@ -209,7 +210,7 @@ public class FrmCreatePatient extends javax.swing.JFrame {
      public void register() {
         Gson gson = new Gson();
         conection=new Conection("patients");
-        conection.insertPhysioterapist(create());
+        conection.insertPatient(create());
         FileManager.save("physioterapist.json", gson.toJson(create()));
     } 
     long id;
